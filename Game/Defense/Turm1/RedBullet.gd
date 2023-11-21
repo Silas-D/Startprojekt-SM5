5da8f58f7a00ebc777d58dfecbe7fb96e2e7734c
Avanti1304 <julian.frodl@gmx.de>
2023-11-21 15:05:13 +0100

    Turm kann schießen+bugfix

--- Game/Defense/Turm1/RedBullet.gd
+++ Game/Defense/Turm1/RedBullet.gd
@@ -1,21 +1,20 @@
 extends CharacterBody2D
 
+
 var target
 var Speed = 1000
 var pathName = ""
-var bulletDamage 
+var bulletDamage
 
 func _physics_process(delta):
+	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
 	
-	var pathSpawnerNode = get_tree().get_root().getnode("Main/PathSpawner")
 	for i in pathSpawnerNode.get_child_count():
 		if pathSpawnerNode.get_child(i).name == pathName:
-			target = pathSpawnerNode.get_chilld(i).get_child(0).get_child(0).global_position
+			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
 	
-
-	velocity = global_position.direction_to(target) *Speed
-
+	velocity = global_position.direction_to(target) * Speed
+	
 	look_at(target)
 	
 	move_and_slide()
-	
