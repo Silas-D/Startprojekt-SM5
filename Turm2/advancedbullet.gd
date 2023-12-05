4e6ded523120d4dec7edab0ef2cde216491c9329
mahmoudlag <148867433+mahmoudlag@users.noreply.github.com>
2023-12-05 12:41:05 +0100

    Add files via upload

---
+++ Turm2/advancedbullet.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+var target
+var Speed = 1000
+var pathName = ""
+var bulletDamage
+
+func _physics_process(delta):
+	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
+	
+	for i in pathSpawnerNode.get_child_count():
+		if pathSpawnerNode.get_child(i).name == pathName:
+			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+	
+	velocity = global_position.direction_to(target) * Speed
+	
+	look_at(target)
+	
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if "Soldier A" in body.name:
+		#body.Health -= bulletDAmage
+		queue_free()
+		
