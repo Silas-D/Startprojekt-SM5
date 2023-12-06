50c8bdbbd250480c07a4ef41f0c8d4ae34e055a2
Silas Dalig <148689531+Silas-D@users.noreply.github.com>
2023-12-06 19:15:24 +0100

    Update Soldier A.gd

--- Game/Enemy/Gegner1/Soldier A.gd
+++ Game/Enemy/Gegner1/Soldier A.gd
@@ -9,6 +9,7 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+		#Hier Game Over einfügen
 	
 	if health <= 0:
 		get_parent().get_parent().queue_free()
