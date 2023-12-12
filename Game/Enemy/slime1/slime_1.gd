1f7c59a2065abf93f171fa8dea317db95487e861
Avanti1304 <julian.frodl@gmx.de>
2023-12-12 12:22:36 +0100

    Soldier entfernt, jetzt Slime und weitere Fixes?

--- Game/Enemy/slime1/slime_1.gd
+++ Game/Enemy/slime1/slime_1.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-const speed = 10 
-@export var health = 3
+
+@export var speed = 250
 
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
-	
-	if health <= 0:
-		get_parent().get_parent().queue_free()
+		
--- Game/PathSpawner.gd
+++ Game/PathSpawner.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
-@onready var path = preload("res://Game/Enemy/Gegner1/Stage1.tscn")
+@onready var path = preload("res://Game/Enemy/slime1/Stage 1.tscn")
 
 func _on_timer_timeout():
 	var tempPath = path.instantiate()
