964aaf8ac2378d1c6d8101f98cb49895e36c2c3c
Avanti1304 <julian.frodl@gmx.de>
2023-12-12 22:51:26 +0100

    Bullet reloading

--- Game/Defense/Turm1/Standard-Tower.gd
+++ Game/Defense/Turm1/Standard-Tower.gd
@@ -7,11 +7,33 @@ var currTargets = []
 var curr
 var towerHealth = 10
 
+var reload = 0
+var range = 400
+
+@onready var timer = get_node("ProgressBar/Timer")
+var startShooting = false
+
 
 func _process(_delta):
+	if is_instance_valid(curr):
+		if timer.is_stopped():
+			timer.start()
+	else:
+		for i in get_node("Node2D/BulletContainer").get_child_count():
+			get_node("Node2D/BulletContainer").get_child(i).queue_free()
+			
 	if towerHealth <= 0:
 		print("Turm down")
 		queue_free()
+
+
+func Shoot():
+	var tempBullet = Bullet.instantiate()
+	tempBullet.pathName = pathName
+	get_node("Node2D/BulletContainer").add_child(tempBullet)
+	tempBullet.global_position = $Node2D/Aim.global_position
+
+
 
 func _on_tower_body_entered(body):
 	if "Slime" in body.name:
@@ -34,12 +56,6 @@ func _on_tower_body_entered(body):
 		curr = currTarget
 		pathName = curr.get_parent().name
 		
-		var tempBullet = Bullet.instantiate()
-		tempBullet.pathName = pathName
-		get_node("Node2D/BulletContainer").add_child(tempBullet)
-		tempBullet.global_position = $Node2D/Aim.global_position
-
-
 func _on_gegner_scan_body_entered(body):
 	
 	if "Slime_1" in body.name:
@@ -60,3 +76,7 @@ func _on_gegner_scan_body_entered(body):
 	if "Slime_6" in body.name:
 		towerHealth -= 6
 		body.health -= 100
+
+
+func _on_timer_timeout():
+	Shoot()
