a31a8e53facb4c3976032bb9236ead0371d518e4
Silas <148689531+Silas-D@users.noreply.github.com>
2023-12-15 16:28:22 +0100

    FIX Tower-Bullet Absturz

--- Game/Defense/Turm1/Standard-Bullet.gd
+++ Game/Defense/Turm1/Standard-Bullet.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
 
-var target
+var target = Vector2.ZERO
 const Speed = 1000
 var pathName = ""
 const bulletDamage = 1
 
 func _physics_process(_delta):
 	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
+	var foundTarget
 	
 	for i in pathSpawnerNode.get_child_count():
 		if pathSpawnerNode.get_child(i).name == pathName:
 			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+			foundTarget = true
 	
-	#Problem, maybe wenn geschossen wird und der Slime in der Sek stirbt
-	velocity = global_position.direction_to(target) * Speed
-	print(velocity)
-	
-	look_at(target)
-	
-	move_and_slide()
+	if foundTarget:
+		velocity = global_position.direction_to(target) * Speed
+		print(velocity)
+		look_at(target)
+		move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
