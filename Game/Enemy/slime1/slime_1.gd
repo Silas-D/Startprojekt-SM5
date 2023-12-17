46f8ad38ac63ad6d4341c9591c52fd1187f53168
Avanti1304 <julian.frodl@gmx.de>
2023-12-17 14:31:48 +0100

    wave implementierung grundgerüst

--- Game/Enemy/slime1/slime_1.gd
+++ Game/Enemy/slime1/slime_1.gd
@@ -1,14 +1,21 @@
-extends CharacterBody2D
+extends PathFollow2D
 
-const speed = 50
+const speed = 250
 @export var health = 3
 
 func _process(delta):
-	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if get_parent().get_progress_ratio() == 1:
+	move(delta)
+
+	#get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	
+	if get_progress_ratio() == 1:
 		queue_free()
-		
+
 	if health <= 0:
 		Money.Gold += 50
 		print("Tod")
+		#noch ändern dann
 		get_parent().get_parent().queue_free()
+
+func move(delta):
+	set_progress(get_progress() + speed * delta)
--- Game/main.gd
+++ Game/main.gd
@@ -1,15 +1,48 @@
 extends Node2D
 
-
 @onready var pause_menu = $pauseMenu
 var paused = false
 
+var current_wave = 0
+var enemies_in_wave = 0
+var map_node
 
+func _ready():
+	map_node = get_node("Map/Path1")
+#	var new_enemy = load("res://Game/Enemy/slime1/slime_1.tscn").instantiate()
+#	map_node.add_child(new_enemy, true)
+#.get_node("Path1")
+	start_next_wave()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("Pause"):
 		pauseMenu();
 		
+##
+## Wave Functions
+##
+func start_next_wave():
+	#-> void
+	var wave_data = retrieve_wave_data()
+	#:
+	await get_tree().create_timer(0.2) ##padding
+	spawn_enemies(wave_data)
+
+func retrieve_wave_data():
+	# -> Array
+	var wave_data = [["slime_1", 0.7], ["slime_1", 0.1]]
+	# : Array
+	current_wave += 1
+	enemies_in_wave = wave_data.size()
+	return wave_data
+
+func spawn_enemies(wave_data):
+	# : Array) -> void
+	for i in wave_data:
+		var new_enemy = load("res://Game/Enemy/slime1/" + i[0] + ".tscn").instantiate()
+		map_node.add_child(new_enemy, true)
+		await get_tree().create_timer(i[1])
+
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
@@ -19,8 +52,6 @@ func pauseMenu():
 		Engine.time_scale = 0
 		
 	paused = !paused
-
-
 
 func _on_pause_button_pressed():
 	pauseMenu();
