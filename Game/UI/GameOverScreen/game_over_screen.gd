7d92877c161ac23458072ddb4ff94525a28e2d13
Avanti1304 <julian.frodl@gmx.de>
2023-12-19 15:54:16 +0100

    Game Over Screen Funktionen FERTIG

--- Game/UI/GameOverScreen/game_over_screen.gd
+++ Game/UI/GameOverScreen/game_over_screen.gd
@@ -1,6 +1,14 @@
 extends CanvasLayer
 
+var waves
+var enemies
 
+func _process(_delta):
+	var main = get_tree().get_root().get_node("Main")
+	if main:
+			waves = main.current_wave
+			waves -= 1
+			enemies = main.enemies_in_wave
 
 func _on_weiterspielen_pressed():
 	get_tree().change_scene_to_file("res://Game/main.tscn")
@@ -8,3 +16,14 @@ func _on_weiterspielen_pressed():
 
 func _on_verlassen_pressed():
 	get_tree().change_scene_to_file("res://Game/UI/mainmenu/main_menu.tscn")
+
+
+func _on_visibility_changed():
+	waves = str(waves)
+	var string1 = String()
+	string1 = (waves + " Wellen geschafft!")
+	$"Panel/Wellen ausgabe".set_text(string1)
+	enemies = str(enemies)
+	var string2 = String()
+	string2 = (enemies + " Gegner gespawned")
+	$"Panel/Gegner killed".set_text(string2)
